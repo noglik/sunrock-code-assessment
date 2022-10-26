446ac024238b85b88e4a4044bf967fbf5db765c1
noglik <yaroslaubuk@gmail.com>
2022-10-26 22:57:55 +0200

    Rocket ship time to reach Moon

--- src/rocket.test.ts
+++ src/rocket.test.ts
@@ -1,8 +1,36 @@
 import { expect } from 'chai';
-import { add } from './rocket';
+import { calculateTimeFromDistance, getDistPerSecond } from './rocket';
 
-describe('add', () => {
-  it('should return sum of two numbers', () => {
-    expect(add(2, 2)).to.be.eql(4);
+describe('Rocket', () => {
+  describe('calculateTime', () => {
+    it('should return int, when distance is fibonachi number', () => {
+      expect(calculateTimeFromDistance(317809)).to.be.eql(25);
+    });
+
+    it('should return float, when distance is not fibonachi number', () => {
+      expect(calculateTimeFromDistance(384400)).to.be.eql(25.339026973088007);
+    });
+
+    it('should return float, when distance is float', () => {
+      expect(calculateTimeFromDistance(15.6)).to.be.eql(4.575);
+    });
+
+    it('should succeed, when distance is between 1 and 3', () => {
+      expect(calculateTimeFromDistance(2.7)).to.be.eql(1.85);
+    });
+
+    it('should succeed, when distance is lower than 1', () => {
+      expect(calculateTimeFromDistance(0.7)).to.be.eql(0.7);
+    });
+  });
+
+  describe('getDistPerSecond', () => {
+    it('should calculate traversed distances, except first element', () => {
+      expect(getDistPerSecond(12)).to.be.eql([3, 6, 11, 19]);
+    });
+
+    it('should calculate traversed distances, when distance is fibonachi number', () => {
+      expect(getDistPerSecond(19)).to.be.eql([3, 6, 11, 19])
+    })
   });
 });
--- src/rocket.ts
+++ src/rocket.ts
@@ -1,1 +1,23 @@
-export const add = (a: number, b: number) => a + b;
+// since we start sequence from 1 and 2(not from classic 0), return does not include first distance
+export const getDistPerSecond = (distance: number, first = 1, second = 2, sum = 3): Array<number> =>
+  sum >= distance
+    ? [sum]
+    : [sum, ...getDistPerSecond(distance, second, first + second, sum + first + second)];
+
+export const getFraction = (min: number, mid: number, max: number) => (mid - min) / (max - min);
+
+export const calculateTimeFromDistance = (distance: number) => {
+  // handle edge cases, when distance < 3
+  if (distance < 3) {
+    return distance <= 1 ? distance : 1 + getFraction(1, distance, 3);
+  }
+
+  const dist = getDistPerSecond(distance);
+  const fullSeconds = dist.length;
+
+  if (distance === dist[fullSeconds - 1]) return fullSeconds + 1;
+
+  const fraction = getFraction(dist[fullSeconds - 2], distance, dist[fullSeconds - 1]);
+
+  return fullSeconds + fraction;
+};
